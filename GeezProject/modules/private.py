ff1111b1daa7fabc22a1be608a85aa3a0ef393a0
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2021-07-24 17:05:12 +0700

    Update private.py

--- GeezProject/modules/private.py
+++ GeezProject/modules/private.py
@@ -51,6 +51,7 @@ Saya Memiliki Banyak Fitur Seperti :
                     InlineKeyboardButton(
                         "💬 Info Updates", url=f"https://t.me/{UPDATES_CHANNEL}"), 
                 ]
+                ]
             ]
         ),
         reply_to_message_id=message.message_id
