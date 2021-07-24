4a45c6fe2463b5109e560b0dc2ec40c11f9b6db6
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2021-07-24 18:06:30 +0700

    Update msg.py

--- GeezProject/modules/msg.py
+++ GeezProject/modules/msg.py
@@ -20,7 +20,7 @@ class Messages():
       HELP_MSG = [
         ".",
 f"""
-**Hey 👋 Selamat datang kembali di {PROJECT_NAME}
+**Hey {message.from_user.mention} Selamat datang kembali di {PROJECT_NAME}
 
 ✣️ {PROJECT_NAME} dapat Memutar Lagu di Voice Chat Group Dengan cara yang Mudah.
 
